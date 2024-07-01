e5cb0204d1cffc1f82dfa4434aeb9e690a393ae1
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-07-01 11:25:18 +0700

    SET UP START SERVER

--- Phase2ServerTerminal/Program.cs
+++ Phase2ServerTerminal/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+
+class Server
+{
+    static async Task Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5713);
+        server.Start();
+        Console.WriteLine("Server is started on port 5713");
+    }
+}

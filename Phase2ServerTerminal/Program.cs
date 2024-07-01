c2eae33250611770402cca66ff64f224f6dce6e3
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-07-01 11:32:31 +0700

    SET UP FOR SENDER CONNECTION

--- Phase2ServerTerminal/Program.cs
+++ Phase2ServerTerminal/Program.cs
@@ -8,5 +8,24 @@ class Server
         TcpListener server = new TcpListener(IPAddress.Any, 5713);
         server.Start();
         Console.WriteLine("Server is started on port 5713.");
+        while (true)
+        {
+            TcpClient client = await server.AcceptTcpClientAsync();
+            _ = Connect(client);
+        }
+    }
+    
+    static async Task Connect(TcpClient client)
+    {
+        IPEndPoint senderEndpoint;
+        NetworkStream stream = client.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        string message = await reader.ReadLineAsync();
+        if(message != "SENDER")
+        {
+            senderEndpoint = (IPEndPoint)client.Client.RemoteEndPoint;
+            Console.WriteLine("Sender Connected");
+        }
+        client.Close();
     }
 }
